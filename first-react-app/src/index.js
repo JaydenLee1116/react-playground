9f913fdb0e0de4d09efed9d81338e6815bc5355b
JaydenLee1116 <vv55adss@gmail.com>
2022-10-14 20:22:45 +0900

    feat: Learn react-hook

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // import Library from './chapter_03/Library';
 // import Clock from './chapter_04/Clock';
 // import CommentList from './chapter_05/CommentList';
-import NotificationList from './chapter_06/NotificationList';
+// import NotificationList from './chapter_06/NotificationList';
+import Accomadate from './chapter_07/Accomodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function tick() {
-  root.render(
-    <React.StrictMode>
-      <NotificationList />
-    </React.StrictMode>
-  );
-}
+root.render(
+  <React.StrictMode>
+    <Accomadate />
+  </React.StrictMode>
+);
 
-setInterval(tick, 1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
