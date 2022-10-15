95ab69891f0a731fb5244be6d591a8a7e1b35b59
JaydenLee1116 <vv55adss@gmail.com>
2022-10-15 15:18:12 +0900

    feat: Learn react-event-handler

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -8,12 +8,13 @@ import reportWebVitals from './reportWebVitals';
 // import Clock from './chapter_04/Clock';
 // import CommentList from './chapter_05/CommentList';
 // import NotificationList from './chapter_06/NotificationList';
-import Accomadate from './chapter_07/Accomodate';
+// import Accomadate from './chapter_07/Accomodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Accomadate />
+    <ConfirmButton />
   </React.StrictMode>
 );
 
